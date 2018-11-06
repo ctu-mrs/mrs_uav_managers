8c545baa4df18aa7bb59463f92e786e08dbb69d4
Tomas Baca <klaxalk@gmail.com>
2018-11-06 16:46:40 +0100

    split GainManager and ConstraintManager]

--- src/gain_manager.cpp
+++ src/gain_manager.cpp
@@ -7,8 +7,6 @@
 #include <mrs_msgs/OdometryDiag.h>
 #include <mrs_msgs/EstimatorType.h>
 #include <mrs_msgs/ControllerStatus.h>
-#include <mrs_msgs/TrackerConstraints.h>
-#include <mrs_msgs/TrackerConstraintsRequest.h>
 
 #include <mrs_lib/Profiler.h>
 #include <mrs_lib/ParamLoader.h>
@@ -48,38 +46,26 @@ namespace mrs_mav_manager
     std::vector<std::string>       gain_names_;
     std::map<std::string, Gains_t> gains;
 
-    std::vector<std::string>                                   constraint_names_;
-    std::map<std::string, mrs_msgs::TrackerConstraintsRequest> constraints;
-
   private:
     std::map<std::string, std::vector<std::string>> map_type_allowed_gains;
     std::map<std::string, std::string>              map_type_fallback_gains;
 
-  private:
-    std::map<std::string, std::vector<std::string>> map_type_allowed_constraints;
-    std::map<std::string, std::string>              map_type_fallback_constraints;
-
   public:
     virtual void onInit();
     bool         callbackSetGains(mrs_msgs::String::Request &req, mrs_msgs::String::Response &res);
-    bool         callbackSetConstraints(mrs_msgs::String::Request &req, mrs_msgs::String::Response &res);
     void         callbackOdometryDiagnostics(const mrs_msgs::OdometryDiagConstPtr &msg);
     void         callbackControllerStatus(const mrs_msgs::ControllerStatusConstPtr &msg);
 
     bool setGains(std::string gains_name);
-    bool setConstraints(std::string constraints_names);
 
     bool stringInVector(const std::string &value, const std::vector<std::string> &vector);
 
   private:
     ros::ServiceServer service_server_set_gains;
-    ros::ServiceServer service_server_set_constraints;
 
     ros::ServiceClient service_client_set_gains;
-    ros::ServiceClient service_client_set_constraints;
 
     ros::Publisher publisher_current_gains;
-    ros::Publisher publisher_current_constraints;
 
   private:
     ros::Subscriber        subscriber_odometry_diagnostics;
@@ -87,16 +73,13 @@ namespace mrs_mav_manager
     mrs_msgs::OdometryDiag odometry_diagnostics;
     std::mutex             mutex_odometry_diagnostics;
 
-    // | ------------- constraint and gain management ------------- |
+    // | ------------- gain management ------------- |
 
   private:
     mrs_msgs::EstimatorType::_type_type last_estimator_type;
 
     void       gainsManagementTimer(const ros::TimerEvent &event);
     ros::Timer gains_management_timer;
-
-    void       constraintsManagementTimer(const ros::TimerEvent &event);
-    ros::Timer constraints_management_timer;
 
     int rate_;
 
@@ -109,10 +92,6 @@ namespace mrs_mav_manager
     std::mutex                 mutex_controller_status;
 
     std::string current_gains;
-
-    // | ------------------ constraint management ----------------- |
-
-    std::string current_constraints;
 
     // | ------------------------ profiler ------------------------ |
   private:
@@ -140,7 +119,6 @@ namespace mrs_mav_manager
     param_loader.load_param("enable_profiler", profiler_enabled_);
 
     param_loader.load_param("gains", gain_names_);
-    param_loader.load_param("constraints", constraint_names_);
 
     param_loader.load_param("estimator_types", estimator_type_names_);
     param_loader.load_param("rate", rate_);
@@ -206,79 +184,16 @@ namespace mrs_mav_manager
       map_type_fallback_gains.insert(std::pair<std::string, std::string>(*it, temp_str));
     }
 
-    // loading constraint names
-    for (it = constraint_names_.begin(); it != constraint_names_.end(); ++it) {
-      ROS_INFO_STREAM("[GainManager]: loading constraints \"" << *it << "\"");
-
-      mrs_msgs::TrackerConstraintsRequest new_constraints;
-
-      param_loader.load_param(*it + "/horizontal/speed", new_constraints.horizontal_speed);
-      param_loader.load_param(*it + "/horizontal/acceleration", new_constraints.horizontal_acceleration);
-      param_loader.load_param(*it + "/horizontal/jerk", new_constraints.horizontal_jerk);
-      param_loader.load_param(*it + "/horizontal/snap", new_constraints.horizontal_snap);
-
-      param_loader.load_param(*it + "/vertical/ascending/speed", new_constraints.vertical_ascending_speed);
-      param_loader.load_param(*it + "/vertical/ascending/acceleration", new_constraints.vertical_ascending_acceleration);
-      param_loader.load_param(*it + "/vertical/ascending/jerk", new_constraints.vertical_ascending_jerk);
-      param_loader.load_param(*it + "/vertical/ascending/snap", new_constraints.vertical_ascending_snap);
-
-      param_loader.load_param(*it + "/vertical/descending/speed", new_constraints.vertical_descending_speed);
-      param_loader.load_param(*it + "/vertical/descending/acceleration", new_constraints.vertical_descending_acceleration);
-      param_loader.load_param(*it + "/vertical/descending/jerk", new_constraints.vertical_descending_jerk);
-      param_loader.load_param(*it + "/vertical/descending/snap", new_constraints.vertical_descending_snap);
-
-      param_loader.load_param(*it + "/yaw/speed", new_constraints.yaw_speed);
-      param_loader.load_param(*it + "/yaw/acceleration", new_constraints.yaw_acceleration);
-      param_loader.load_param(*it + "/yaw/jerk", new_constraints.yaw_jerk);
-      param_loader.load_param(*it + "/yaw/snap", new_constraints.yaw_snap);
-
-      constraints.insert(std::pair<std::string, mrs_msgs::TrackerConstraintsRequest>(*it, new_constraints));
-    }
-
-    // loading the allowed constraints lists
-    for (it = estimator_type_names_.begin(); it != estimator_type_names_.end(); ++it) {
-
-      std::vector<std::string> temp_vector;
-      param_loader.load_param("constraint_management/allowed_constraints/" + *it, temp_vector);
-
-      std::vector<std::string>::iterator it2;
-      for (it2 = temp_vector.begin(); it2 != temp_vector.end(); ++it2) {
-        if (!stringInVector(*it2, constraint_names_)) {
-          ROS_ERROR("[GainManager]: the element '%s' of %s_allowed_constraints is not a valid constraint!", it2->c_str(), it->c_str());
-          ros::shutdown();
-        }
-      }
-
-      map_type_allowed_constraints.insert(std::pair<std::string, std::vector<std::string>>(*it, temp_vector));
-    }
-
-    // loading the fallback constraints
-    for (it = estimator_type_names_.begin(); it != estimator_type_names_.end(); ++it) {
-
-      std::string temp_str;
-      param_loader.load_param("constraint_management/fallback_constraints/" + *it, temp_str);
-
-      if (!stringInVector(temp_str, map_type_allowed_constraints.at(*it))) {
-        ROS_ERROR("[GainManager]: the element '%s' of %s_allowed_constraints is not a valid constraint!", temp_str.c_str(), it->c_str());
-        ros::shutdown();
-      }
-
-      map_type_fallback_constraints.insert(std::pair<std::string, std::string>(*it, temp_str));
-    }
-
     ROS_INFO("[GainManager]: done loading dynamical params");
 
     current_gains       = "";
-    current_constraints = "";
     last_estimator_type = -1;
 
     // | ------------------------ services ------------------------ |
 
     service_server_set_gains       = nh_.advertiseService("set_gains_in", &GainManager::callbackSetGains, this);
-    service_server_set_constraints = nh_.advertiseService("set_constraints_in", &GainManager::callbackSetConstraints, this);
 
     service_client_set_gains       = nh_.serviceClient<dynamic_reconfigure::Reconfigure>("set_gains_out");
-    service_client_set_constraints = nh_.serviceClient<mrs_msgs::TrackerConstraints>("set_constraints_out");
 
     // | ----------------------- subscribers ---------------------- |
     subscriber_odometry_diagnostics =
@@ -288,12 +203,10 @@ namespace mrs_mav_manager
     // | ----------------------- publishers ----------------------- |
 
     publisher_current_gains       = nh_.advertise<std_msgs::String>("current_gains_out", 1);
-    publisher_current_constraints = nh_.advertise<std_msgs::String>("current_constraints_out", 1);
 
     // | ------------------------- timers ------------------------- |
 
     gains_management_timer       = nh_.createTimer(ros::Rate(rate_), &GainManager::gainsManagementTimer, this);
-    constraints_management_timer = nh_.createTimer(ros::Rate(rate_), &GainManager::constraintsManagementTimer, this);
 
     // --------------------------------------------------------------
     // |                          profiler                          |
@@ -327,6 +240,7 @@ namespace mrs_mav_manager
     it = gains.find(gains_name);
 
     if (it == gains.end()) {
+      ROS_WARN("[GainManager]: Can't set gains for '%s', the mode is not on a list!", gains_name.c_str());
       return false;
     }
 
@@ -412,30 +326,6 @@ namespace mrs_mav_manager
     ROS_INFO("[GainManager]: setting up gains for '%s'", gains_name.c_str());
 
     return true;
-  }
-
-  //}
-
-  /* setConstraints() //{ */
-
-  bool GainManager::setConstraints(std::string constraints_names) {
-
-    std::map<std::string, mrs_msgs::TrackerConstraintsRequest>::iterator it;
-    it = constraints.find(constraints_names);
-
-    if (it == constraints.end()) {
-      return false;
-    }
-
-    mrs_msgs::TrackerConstraints new_constraints;
-
-    new_constraints.request = it->second;
-
-    service_client_set_constraints.call(new_constraints);
-
-    current_constraints = constraints_names;
-
-    return new_constraints.response.success;
   }
 
   //}
@@ -534,52 +424,6 @@ namespace mrs_mav_manager
 
   //}
 
-  /* //{ callbackSetGains() */
-
-  bool GainManager::callbackSetConstraints(mrs_msgs::String::Request &req, mrs_msgs::String::Response &res) {
-
-    if (!is_initialized)
-      return false;
-
-    char message[200];
-
-    if (!stringInVector(req.value, constraint_names_)) {
-
-      sprintf((char *)&message, "The constraints '%s' do not exist (in the gain_manager's config).", req.value.c_str());
-      res.message = message;
-      res.success = false;
-      ROS_ERROR("[GainManager]: %s", message);
-      return true;
-    }
-
-    if (!stringInVector(req.value, map_type_allowed_constraints.at(odometry_diagnostics.estimator_type.name))) {
-
-      sprintf((char *)&message, "The constraints '%s' are not allowed given the current odometry.type.", req.value.c_str());
-      res.message = message;
-      res.success = false;
-      ROS_ERROR("[GainManager]: %s", message);
-      return true;
-    }
-
-    // try to set the gains
-    if (!setConstraints(req.value)) {
-
-      res.message = "the control_manager can't set the constraints";
-      res.success = false;
-      return true;
-
-    } else {
-
-      sprintf((char *)&message, "The constraints '%s' are set.", req.value.c_str());
-      res.message = message;
-      res.success = true;
-      ROS_INFO("[GainManager]: %s", message);
-      return true;
-    }
-  }
-
-  //}
-
   // --------------------------------------------------------------
   // |                           timers                           |
   // --------------------------------------------------------------
@@ -602,7 +446,7 @@ namespace mrs_mav_manager
       std::scoped_lock lock(mutex_controller_status);
 
       if (!(got_controller_status && controller_status.controller.compare("mrs_controllers/So3Controller") == STRING_EQUAL)) {
-        ROS_WARN_THROTTLE(1.0, "[GainManager]: can't do gain management, the NSF controller is not running!");
+        ROS_WARN_THROTTLE(1.0, "[GainManager]: can't do gain management, the SO3 controller is not running!");
         return;
       }
     }
@@ -616,9 +460,7 @@ namespace mrs_mav_manager
       it = map_type_fallback_gains.find(odometry_diagnostics.estimator_type.name);
 
       if (it == map_type_fallback_gains.end()) {
-
         ROS_ERROR("[GainManager]: the odometry.type %s was not specified in the gain_manager's config!", odometry_diagnostics.estimator_type.name.c_str());
-
       } else {
         if (setGains(it->second)) {
           last_estimator_type = odometry_diagnostics.estimator_type.type;
@@ -634,54 +476,6 @@ namespace mrs_mav_manager
     }
     catch (...) {
       ROS_ERROR("Exception caught during publishing topic %s.", publisher_current_gains.getTopic().c_str());
-    }
-  }
-
-  //}
-
-  /* constraintsManagementTimer() //{ */
-
-  void GainManager::constraintsManagementTimer(const ros::TimerEvent &event) {
-
-    if (!is_initialized)
-      return;
-
-    mrs_lib::Routine profiler_routine = profiler->createRoutine("constraintsManagementTimer", rate_, 0.01, event);
-
-    if (!got_odometry_diagnostics) {
-      ROS_WARN_THROTTLE(1.0, "[GainManager]: can't do constrint management, missing odometry diagnostics!");
-      return;
-    }
-
-    // | --- automatically set constraints when odometry.type schanges -- |
-    if (odometry_diagnostics.estimator_type.type != last_estimator_type) {
-
-      ROS_WARN("[GainManager]: the odometry.type has changed! %d -> %d", last_estimator_type, odometry_diagnostics.estimator_type.type);
-
-      std::map<std::string, std::string>::iterator it;
-
-      it = map_type_fallback_constraints.find(odometry_diagnostics.estimator_type.name);
-
-      if (it == map_type_fallback_constraints.end()) {
-
-        ROS_ERROR("[GainManager]: the odometry.type %s was not specified in the constraint_manager's config!",
-                  odometry_diagnostics.estimator_type.name.c_str());
-
-      } else {
-        if (setConstraints(it->second)) {
-          last_estimator_type = odometry_diagnostics.estimator_type.type;
-        }
-      }
-    }
-
-    std_msgs::String str_out;
-    str_out.data = current_constraints;
-
-    try {
-      publisher_current_constraints.publish(str_out);
-    }
-    catch (...) {
-      ROS_ERROR("Exception caught during publishing topic %s.", publisher_current_constraints.getTopic().c_str());
     }
   }
 
