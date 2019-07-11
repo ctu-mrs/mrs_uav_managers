95ab7f574326b6c2f1dcbbbd67ca6c53773988cb
Tomas Baca <tomas.baca@fel.cvut.cz>
2019-07-11 17:25:48 +0200

    updating controllers even when they are not active

--- src/control_manager.cpp
+++ src/control_manager.cpp
@@ -4631,8 +4631,21 @@ void ControlManager::updateControllers(nav_msgs::Odometry odom_for_control) {
   if (last_position_cmd != mrs_msgs::PositionCommand::Ptr()) {
 
     try {
-      // TODO we should update the inactive controllers aswell
-      controller_output_cmd = controller_list[active_controller_idx]->update(odometry_const_ptr, last_position_cmd);
+
+      // for each controller
+      for (unsigned int i = 0; i < controller_list.size(); i++) {
+
+        if ((int)i == active_controller_idx) {
+
+          // if it is the active one, update and retrieve the command
+          controller_output_cmd = controller_list[active_controller_idx]->update(odometry_const_ptr, last_position_cmd);
+
+        } else {
+
+          // if it is not the active one, just update without retrieving the command
+          controller_list[i]->update(odometry_const_ptr, last_position_cmd);
+        }
+      }
 
       // in normal sitation, the controller returns a valid command
       if (controller_output_cmd != mrs_msgs::AttitudeCommand::Ptr()) {
