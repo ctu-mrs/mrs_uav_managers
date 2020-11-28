e12e9aec8731add80938ec53f77362a15795c766
Tomas Baca <klaxalk@gmail.com>
2020-11-28 12:22:07 +0100

    added github actions file, removed travis

--- .ci/ci_before_install.sh
+++ .ci/ci_before_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# author: Robert Penicka
 set -e
 
 distro=`lsb_release -r | awk '{ print $2 }'`
@@ -8,10 +7,11 @@ distro=`lsb_release -r | awk '{ print $2 }'`
 
 echo "Starting install preparation" 
 
-openssl aes-256-cbc -K $encrypted_f0fd3ee254e8_key -iv $encrypted_f0fd3ee254e8_iv -in ./.ci/deploy_key_github.enc -out ./.ci/deploy_key_github -d
-eval "$(ssh-agent -s)"
-chmod 600 ./.ci/deploy_key_github
-ssh-add ./.ci/deploy_key_github
+# get the current commit SHA
+SHA=`git rev-parse HEAD`
+
+# get the current package name
+PACKAGE_NAME=${PWD##*/}
 
 sudo apt-get -y update -qq
 sudo apt-mark hold openssh-server
@@ -34,13 +34,6 @@ echo "running the main install.sh"
 ./installation/install.sh
 
 gitman update
-
-# get the current commit SHA
-cd "$TRAVIS_BUILD_DIR"
-SHA=`git rev-parse HEAD`
-
-# get the current package name
-PACKAGE_NAME=${PWD##*/}
 
 # checkout the SHA
 cd ~/uav_core/.gitman/$PACKAGE_NAME
