60d694c86f3a89aecf33cff1bdf5382a08000aff
Tomas Baca <klaxalk@gmail.com>
2020-11-28 12:51:17 +0100

    build matrix

--- .ci/install.sh
+++ .ci/install.sh
@@ -5,7 +5,7 @@ distro=`lsb_release -r | awk '{ print $2 }'`
 [ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
 [ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
 
-echo "Starting install preparation" 
+echo "Starting install" 
 
 # get the current commit SHA
 SHA=`git rev-parse HEAD`
@@ -45,4 +45,4 @@ ln -s ~/uav_core
 source /opt/ros/$ROS_DISTRO/setup.bash
 cd ~/catkin_ws
 
-echo "install part ended"
+echo "install ended"
