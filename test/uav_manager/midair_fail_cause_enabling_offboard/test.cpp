6ef6856dc8227794fb15f5642ff0b90b8ada440d
Tomas Baca <klaxalk@gmail.com>
2024-01-08 18:47:29 +0100

    added midair failure test

---
+++ test/uav_manager/midair_fail_cause_enabling_offboard/test.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+
+#include <mrs_uav_testing/test_generic.h>
+
+class Tester : public mrs_uav_testing::TestGeneric {
+
+public:
+  bool test();
+};
+
+bool Tester::test() {
+
+  {
+    auto [success, message] = activateMidAir();
+
+    if (success) {
+      ROS_ERROR("[%s]: midair activation succeeded, which should not happen: '%s'", ros::this_node::getName().c_str(), message.c_str());
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+TEST(TESTSuite, test) {
+
+  Tester tester;
+
+  bool result = tester.test();
+
+  if (result) {
+    GTEST_SUCCEED();
+  } else {
+    GTEST_FAIL();
+  }
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
+
+  ros::init(argc, argv, "test");
+
+  testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
