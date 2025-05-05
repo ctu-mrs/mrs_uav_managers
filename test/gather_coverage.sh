dfc59b2498c2ffdbd59792bd9115c6bb9c42b7aa
Tomas Baca <Tomas Baca>
2025-05-05 14:11:09 +0200

    fixed coverage gatherer

--- test/gather_coverage.sh
+++ test/gather_coverage.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-while [ ! -e ".catkin_tools" ]; do
+while [ ! -e "build/COLCON_IGNORE" ]; do
   cd ..
   if [[ `pwd` == "/" ]]; then
     # we reached the root and didn't find the build/COLCON_IGNORE file - that's a fail!
-    echo "$0: could not find the root of the current workspace".
-    return 1
+    echo "Cannot compile, probably not in a workspace (if you want to create a new workspace, call \"colcon init\" in its root first)".
+    exit 1
   fi
 done
 
@@ -13,7 +13,7 @@ WORKSPACE_NAME=${PWD##*/}
 
 cd build
 
-lcov --capture --directory . --output-file coverage.info
+lcov --ignore-errors negative --capture --directory . --output-file coverage.info
 lcov --remove coverage.info "*/test/*" --output-file coverage.info.removed
 lcov --extract coverage.info.removed "*/${WORKSPACE_NAME}/src/*" --output-file coverage.info.cleaned
 genhtml --title "MRS UAV System - Test coverage report" --demangle-cpp --legend --frames --show-details -o coverage_html coverage.info.cleaned | tee /tmp/genhtml.log
