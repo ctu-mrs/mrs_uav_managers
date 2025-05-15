cf0e8e7d0ea36235bee1a6faa8235abcf27702ac
Tomas Baca <Tomas Baca>
2025-05-15 17:24:35 +0200

    + takeoff test

--- test/run_specific_test.sh
+++ test/run_specific_test.sh
@@ -11,6 +11,6 @@ done
 
 colcon test-result --delete-yes
 
-colcon test --packages-select mrs_uav_managers --ctest-args -R 'max_throttle_check' --event-handlers console_direct+ console_stderr- console_start_end-
+colcon test --packages-select mrs_uav_managers --ctest-args -R 'takeoff_service' --event-handlers console_direct+ console_stderr- console_start_end-
 
 colcon test-result --all --verbose
