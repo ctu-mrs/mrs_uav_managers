0ee90ce5f8663978286f52e365a75e7ae0c2ea29
Tomas Baca <Tomas Baca>
2025-05-22 15:49:52 +0200

    + uav manager tests

---
+++ test/uav_manager/takeoff_service_fail_cause_tracker/test.cpp
@@ -0,0 +1,29 @@
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/time.hpp>
+
+#include <mrs_uav_testing/test_generic.h>
+
+#include <failed_takeoff_test.h>
+
+using namespace std::chrono_literals;
+
+class Tester : public FailedTakeoffTest {
+
+public:
+  Tester() : FailedTakeoffTest(){};
+};
+
+int main(int argc, char* argv[]) {
+
+  rclcpp::init(argc, argv);
+
+  bool test_result = true;
+
+  Tester tester;
+
+  test_result &= tester.test();
+
+  tester.reportTestResult(test_result);
+
+  tester.join();
+}
