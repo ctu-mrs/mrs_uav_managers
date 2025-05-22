d92f26fa585fbdb7647b2a9fdab3bc9e383f7f2c
Tomas Baca <Tomas Baca>
2025-05-22 16:25:17 +0200

    + uav_manager tests

---
+++ test/uav_manager/midair_service_fail_cause_controller/test.cpp
@@ -0,0 +1,72 @@
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/time.hpp>
+
+#include <mrs_uav_testing/test_generic.h>
+
+using namespace std::chrono_literals;
+
+class Tester : public mrs_uav_testing::TestGeneric {
+
+public:
+  Tester() : mrs_uav_testing::TestGeneric() {
+  }
+
+  bool test(void);
+};
+
+bool Tester::test(void) {
+
+  std::shared_ptr<mrs_uav_testing::UAVHandler> uh;
+
+  const std::string uav_name = "uav1";
+
+  {
+    auto [uhopt, message] = getUAVHandler(uav_name);
+
+    if (!uhopt) {
+      RCLCPP_ERROR(node_->get_logger(), "Failed obtain handler for '%s': '%s'", uav_name.c_str(), message.c_str());
+      return false;
+    }
+
+    uh = uhopt.value();
+  }
+
+  // | ---------- call midair activation in async task ---------- |
+
+  {
+    auto [success, message] = uh->activateMidAir();
+
+    if (success) {
+      RCLCPP_ERROR(node_->get_logger(), "midair activation succeeded, this should not happen: '%s'", message.c_str());
+      return false;
+    }
+  }
+
+  // | -------------------------- wait -------------------------- |
+
+  sleep(1.0);
+
+  // | -------------- check if we are in emergency -------------- |
+
+  if (!uh->isFlyingNormally() && uh->getActiveController() == "EmergencyController" && uh->getActiveTracker() == "LandoffTracker") {
+    return true;
+  } else {
+    RCLCPP_ERROR(node_->get_logger(), "not in the ehover state");
+    return false;
+  }
+}
+
+int main(int argc, char* argv[]) {
+
+  rclcpp::init(argc, argv);
+
+  bool test_result = true;
+
+  Tester tester;
+
+  test_result &= tester.test();
+
+  tester.reportTestResult(test_result);
+
+  tester.join();
+}
