84aca030ed559dff9425aaab6735c6957742d130
Tomas Baca <Tomas Baca>
2025-12-10 11:50:20 +0100

    fixed test for rc controled bumper

--- test/run_specific_test.sh
+++ test/run_specific_test.sh
@@ -13,6 +13,6 @@ export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 
 colcon test-result --delete-yes
 
-colcon test --packages-select mrs_uav_managers --ctest-args -R 'min_height_check' --event-handlers console_direct+ console_stderr- console_start_end-
+colcon test --packages-select mrs_uav_managers --ctest-args -R 'bumper_with_rc_control' --event-handlers console_direct+ console_stderr- console_start_end-
 
 colcon test-result --all --verbose
